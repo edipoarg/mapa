9ac6ac8b622048b3b4283b0d34c328bc7bd3ec71
anon author <noemail@nothing.null>
2024-04-27 13:10:14 -0300

    Main styles and routes TODO: rethink overley method not to load map twice

--- src/main.jsx
+++ src/main.jsx
@@ -64,7 +64,7 @@ const router = createHashRouter([
       { path: "/autorxs", element: <Autorxs/>},
       { path: "/investigacion/:dominio", element: <Investigacion/>},
       { path: '/investigaciones/lista', element: <TodasInvestigaciones /> },
-      { path: "/ficha/:NumCaso", element: <FichaDependencias /> 
+      { path: "/ficha/:Contador", element: <FichaDependencias /> 
     }
     ]
   },
