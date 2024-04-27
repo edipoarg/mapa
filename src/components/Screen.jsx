5f56e64f2cf1053ce46c8954c7ebbff741a10519
anon author <noemail@nothing.null>
2024-04-27 13:20:10 -0300

    #7 - Pop up is currently Screen.jsx TODO: Rename and complete data visualisation and style WORK IN PROGRESS

--- src/components/Screen.jsx
+++ src/components/Screen.jsx
@@ -1,6 +1,17 @@
 import styles from "../styles/Screen.module.css";
+import {Link} from "react-router-dom";
 
-const Screen = ({ title, level, adress, phone, age, circs }) => {
+// Función para recortar texto si supera el límite de caracteres
+const truncateText = (text, maxLength) => {
+  // Asegúrate de que el texto no sea null ni undefined
+  const safeText = text ?? ''; // Usa una cadena vacía si text es null o undefined
+  if (safeText.length > maxLength) {
+    return safeText.substring(0, maxLength) + '...';
+  }
+  return safeText;
+};
+
+const Screen = ({ title, level, adress, phone, age, circs, caseId }) => {
   return (
     <section className={styles.Screen}>
       <section className={styles.ComisariaScreen}>
@@ -10,8 +21,11 @@ const Screen = ({ title, level, adress, phone, age, circs }) => {
           <h4 className={styles.adress}>{adress}</h4>
           <h4 className={styles.phone}>{phone}</h4>
           <h4 className={styles.age}>{age}</h4>
-          <h4 className={styles.circs}>{circs}</h4>
+          <h4 className={styles.circs}>{truncateText(circs, 120)}</h4>
 
+          <Link to={`/ficha/${caseId}`}>
+          <h3 className={styles.moreButton}>Ver +</h3>
+          </Link>
 
         </section>
       </section>
